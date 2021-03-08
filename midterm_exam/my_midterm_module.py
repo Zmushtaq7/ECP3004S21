5477bc49f9ca9ff2cfd451853faf3531c19b85d7
Zmushtaq7 <zaidmushtaq97@gmail.com>
2021-03-08 17:36:24 -0500

    Finished midterm

--- midterm_exam/my_midterm_module.py
+++ midterm_exam/my_midterm_module.py
@@ -4,9 +4,9 @@
 #
 # ECP 3004: Python for Business Analytics
 #
-# Name: 
+# Name: Zaid Mushtaq 
 #
-# Date:
+# Date: 3/8/21
 # 
 ##################################################
 #
@@ -39,6 +39,7 @@
 ##################################################
 
 # import name_of_module
+import numpy as np
 
 
 ##################################################
@@ -50,7 +51,7 @@
 # Exercise 1
 
 
-def function_name(x: type) -> type:
+def in_budget(x:int, y:int, p_x:float, p_y:float, w:float) -> bool:
     """
     Preconditions: x, y, w >= 0 and p_x, p_y > 0
     
@@ -60,21 +61,23 @@ def function_name(x: type) -> type:
     
     >>> in_budget(3, 1, 10, 25, 100)
     True
-    >>> function_name(arguments)
-    answer_you_expect
-    >>> function_name(arguments)
-    answer_you_expect
+    >>> in_budget(2, 2, 15, 20, 100)
+    True
+    >>> in_budget(5, 3, 10, 25, 100)
+    False
     
     """
-    
-    return None
+    total_cost = (x*p_x)+(y*p_y)
+    if w >= total_cost:
+        return True
+    return False
 
 
 
 # Exercise 2
 
 
-def function_name(x: type) -> type:
+def calc_bundle(p_x, p_y, w, alpha) -> type:
     """
     Preconditions: w >= 0 and p_x, p_y > 0 and 0 <= alpha <= 1
     
@@ -83,21 +86,23 @@ def function_name(x: type) -> type:
     
     >>> calc_bundle(10, 20, 120, 1.0/3.0)
     [4.0, 4.0]
-    >>> function_name(arguments)
-    answer_you_expect
-    >>> function_name(arguments)
-    answer_you_expect
+    >>> calc_bundle(10, 25, 100, .5)
+    [5.0, 2.0]
+    >>> calc_bundle(10, 25, 100, 2.0/3.0)
+    [6.667, 1.333]
     
     """
+    x_star = (alpha/p_x)*w
+    y_star = ((1-alpha)/p_y)*w
     
-    return None
+    return [x_star, y_star]
 
 
 
 # Exercise 3
 
 
-def function_name(x: type) -> type:
+def y_solve(x_star, p_x, p_y, w) -> type:
     """
     Preconditions: w >= 0 and p_x, p_y > 0 and 0 <= x_star <= w/p_x
     
@@ -106,20 +111,20 @@ def function_name(x: type) -> type:
     
     >>> y_solve(5, 5, 10, 100)
     7.5
-    >>> function_name(arguments)
-    answer_you_expect
-    >>> function_name(arguments)
-    answer_you_expect
+    >>> y_solve(5, 10, 25, 100)
+    2.0
+    >>> y_solve(7, 10, 25, 100)
+    1.2
     
     """
     
-    return None
+    return (w-x_star*p_x)/p_y
 
 
 # Exercise 4
 
 
-def function_name(x: type) -> type:
+def one_loop_bundle(p_x, p_y, w, alpha, step) -> type:
     """
     Preconditions: w >= 0 and p_x, p_y > 0 and 0 <= alpha <= 1
     
@@ -130,21 +135,31 @@ def function_name(x: type) -> type:
     
     >>> one_loop_bundle(5, 10, 100, 0.25, 0.01)
     [5.0, 7.5]
-    >>> function_name(arguments)
-    answer_you_expect
-    >>> function_name(arguments)
-    answer_you_expect
+    >>> one_loop_bundle(10, 25, 100, .5, .1)
+    [5.0, 2.0]
+    >>> one_loop_bundle(25, 25, 100, .5, .1)
+    [2.0, 2.0]
     
     """
-    
-    return None
+    max_util = -99999
+    x_list = np.arange(0, int(w/p_x), step)
+    y_star = 0
+    x_star = 0
+    for x in x_list:
+        y_candidate = y_solve(x, p_x, p_y, w)
+        if max_util < x**alpha*y_candidate**(1-alpha):
+            max_util = x**alpha*y_candidate**(1-alpha)
+            x_star = x
+            y_star = y_candidate
+        
+    return [x_star, y_star]
 
 
 
 # Exercise 5
 
 
-def function_name(x: type) -> type:
+def util_in_budget(x, y, p_x, p_y, w, alpha) -> type:
     """Calculates the value of the Cobb-Douglass utility
     function for consumption goods x and y with exponent alpha.
     It restricts x and y to non-negative values and 
@@ -154,14 +169,21 @@ def function_name(x: type) -> type:
     
     >>> util_in_budget(4, 4, 10, 20, 120, 1.0/3.0)
     4.0
-    >>> function_name(arguments)
-    answer_you_expect
-    >>> function_name(arguments)
-    answer_you_expect
-    
+    >>> util_in_budget(2, 2, 25, 25, 100, .5)
+    2.0
+    >>> util_in_budget(-1, 4, 100, 100, 100, .5)
+    0.0
+    >>> util_in_budget(4, -1, 100, 100, 100, .5)
+    0.0
+    >>> util_in_budget(4, 4, 100, 100, 100, 1.1)
+    0.0    
     """
     
-    return None
+    if x <= 0 or y <= 0 or alpha > 1.0 or alpha < 0.0:
+        return 0.0
+    elif in_budget(x, y, p_x, p_y, w):
+        return x**alpha*y**(1-alpha)
+    return 0.0
 
 
 
@@ -170,7 +192,7 @@ def function_name(x: type) -> type:
 # Exercise 6
 
 
-def function_name(x: type) -> type:
+def two_loop_bundle(p_x, p_y, w, alpha, step) -> type:
     """
     Preconditions: w >= 0 and p_x, p_y > 0 and 0 <= alpha <= 1
     
@@ -183,14 +205,26 @@ def function_name(x: type) -> type:
     
     >>> two_loop_bundle(10, 25, 100, 0.5, 0.01)
     [5.0, 2.0]
-    >>> function_name(arguments)
-    answer_you_expect
-    >>> function_name(arguments)
-    answer_you_expect
-    
+    >>> two_loop_bundle(25, 25, 100, .5, .01)
+    [2.0, 2.0]
+    >>> two_loop_bundle(10, 20, 120, 1.0/3.0, .01)
+    [4.0, 4.0] 
     """
     
-    return None
+    x_star_list = np.arange(0, w/p_x, step)
+    y_star_list = np.arange(0, w/p_y, step)
+    i_max = 0
+    j_max = 0
+    max_util = 0
+    for i in range(len(x_star_list)):
+        for j in range(len(y_star_list)):
+            cur_util = util_in_budget(x_star_list[i], y_star_list[j], p_x, p_y, w, alpha)
+            if cur_util > max_util:
+                max_util = cur_util
+                i_max = i
+                j_max = j
+    
+    return [x_star_list[i_max], y_star_list[j_max]]
 
 
 
@@ -219,8 +253,9 @@ def function_name(x: type) -> type:
 # not when it is imported. 
 
 
-
-
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
 
 
